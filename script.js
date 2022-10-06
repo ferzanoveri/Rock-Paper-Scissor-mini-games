45c37b558a0e4430d3bad1a0a22eacc4a5cbe742
Ferza Noveri <110340182+ferzanoveri@users.noreply.github.com>
2022-10-06 09:04:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+class Start{
+    constructor(){
+        this.playerName = "Player"
+        this.botName = "Bot"
+        this.playerOption;
+        this.botOption;
+        this.winner = ""
+    }
+
+    get getBotOption() {
+        return this.botOption;
+    }
+
+    set setBotOption(option) {
+        this.botOption = option;
+    }
+
+    get getPlayerOption() {
+        return this.playerOption;
+    }
+
+    set setPlayerOption (option) {
+        this.playerOption = option;
+    }
+
+    botBrain() {
+        const option = ["✋", "✊", "✌️"];
+        const bot = option[Math.floor(Math.random() * option.length)];
+        return bot;
+    }
+
+    winCalculation() {
+        if (this.botOption == "✋" && this.playerOption == "✌️") {
+            return this.winner = this.playerName;
+        }else if (this.botOption == "✋" && this.playerOption == "✊") {
+            return this.winner = this.botName;
+        }else if (this.botOption == "✌️" && this.playerOption == "✋") {
+            return this.winner = this.botName;
+        }else if (this.botOption == "✌️" && this.playerOption == "✊") {
+            return this.winner = this.playerName;
+        }else if (this.botOption == "✊" && this.playerOption == "✋") {
+            return this.winner = this.playerName;
+        }else if (this.botOption == "✊" && this.playerOption == "✌️") {
+            return this.winner = this.botName;
+        }else {
+            return this.winner = "SERI";
+        }
+    }
+
+    matchResult() {
+        if (this.winner != "SERI") {
+            return `${this.winner} MENANG!`;
+        }else {
+            return `WAAA ${this.winner}, GAK ADA YG MENANG 🤪`;
+        }
+    }
+
+}
+
+function pickOption(params) {
+    const start = new Start();
+    start.setPlayerOption = params;
+    start.setBotOption = start.botBrain();
+    start.winCalculation();
+
+    const inGame = document.getElementById("inGame");
+    const result = document.getElementById("result");
+
+    inGame.textContent = "..."
+    result.textContent = "..."
+
+    setTimeout(() => {
+        inGame.textContent = `${start.getPlayerOption} VS ${start.getBotOption}`;
+        result.textContent = start.matchResult()
+    }, 1500);
+
+}
